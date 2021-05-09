07de8e1da06ab240595819dfc9ced000662a8d2d
Jason Zhen <jasonzhen.mail@gmail.com>
2021-05-09 01:23:53 -0700

    Complete first half of phonebook exercises

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 const Filter = ({ filter, handleFilterChange }) => {
   return (
@@ -50,16 +52,22 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons ] = useState([]);
   const [newPerson, setNewPerson] = useState(
     { name: '', number: ''}
   )
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+      })
+  }, [])
+  console.log('render', persons.length, 'persons');
 
   const addPerson = (event) => {
     event.preventDefault();
