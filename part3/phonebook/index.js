2acd814b9b27003340f1f25d5dac37a6d191d90c
Jason Zhen <jasonzhen.mail@gmail.com>
2021-05-09 17:36:49 -0700

    Add frontend build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('request-body', (req, res) => {
   return JSON.stringify(req.body)
